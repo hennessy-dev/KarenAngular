9cf2f614563cac0ed560bb14d15f922cf358be70
hhennessyy <juandavidytm1@gmail.com>
2024-01-28 18:39:06 -0500

    feat: :sparkles: TailwindCSS and Flowbite components configurated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Karen';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
